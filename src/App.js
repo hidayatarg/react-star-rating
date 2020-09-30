6b9662472aca257a8069a88b846b1c4310df5e00
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-09-30 15:21:56 +0300

    Star rating implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+const Star = (props) => {
+  const {starId, rating, onMouseEnter, onMouseLeave, onClick} = props
+  let styleClass = 'star-rating-blank'
+  if (rating > starId){
+    styleClass = 'star-rating-filled'
+  }
+  console.log('gelen rating to start component: ', rating)
+  return (
+    <div
+      className="star"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
+    >
+      <svg
+        height="55px"
+        width="53px"
+        className={styleClass}
+        viewBox="0 0 25 23"
+        data-rating="1"
+      >
+        <polygon
+          strokeWidth="0"
+          points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
+        />
+      </svg>
+    </div>
+  );
+}
+
+
 function App() {
+  // using hooks 
+  const [rating, setRating] = useState(0)
+  const [hoverState, setHoverState] = useState(0)
+  // using 5 stars rating
+  const stars = [1, 2, 3, 4, 5]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stars Rating</h1>
+        <div className="flex-container">
+          {
+            stars.map((star, i) => (
+              <Star
+                key={i}
+                starId={i}
+                rating={hoverState || rating} 
+                onMouseEnter={() => setHoverState(i+1)}
+                onMouseLeave={() => setHoverState(0)}
+                onClick={() => setRating(i+1)}
+              />
+            ))
+          }
+        </div>
       </header>
     </div>
   );

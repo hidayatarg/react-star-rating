f9e15828d77cb34efbfcb7b7cc0018bf751b62bf
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-10-21 12:59:14 +0300

    Updates

---
+++ precache-manifest.eb18d0d6021c2150861726906e1deb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776f37ffb7b80bd8475efb14d5dfe331",
+    "url": "/react-star-rating/index.html"
+  },
+  {
+    "revision": "8d77ba936caa80b917d6",
+    "url": "/react-star-rating/static/css/main.26ea57e6.chunk.css"
+  },
+  {
+    "revision": "c20cd81eae99a24da722",
+    "url": "/react-star-rating/static/js/2.88ef11cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-star-rating/static/js/2.88ef11cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d77ba936caa80b917d6",
+    "url": "/react-star-rating/static/js/main.1dbcf15f.chunk.js"
+  },
+  {
+    "revision": "b43980eb5202e11e7a2e",
+    "url": "/react-star-rating/static/js/runtime-main.6f516e2a.js"
+  }
+]);
